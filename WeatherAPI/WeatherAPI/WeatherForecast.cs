7ad57ee5c5758f9111dac854741d08d5a36530a1
Wisdou <51473681+Wisdou@users.noreply.github.com>
2022-09-02 23:21:09 +0300

    Add class for weather parameters

--- WeatherAPI/WeatherAPI/WeatherForecast.cs
+++ WeatherAPI/WeatherAPI/WeatherForecast.cs
@@ -6,10 +6,10 @@ namespace WeatherAPI
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public double TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public double TemperatureF => 32 + (TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public double Humidity { get; set; }
     }
 }
